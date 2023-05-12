b83bd53be05519c5e4aeb123e9b28e1ad5ebb144
Iain Simmons <emptiful@gmail.com>
2023-05-13 09:35:26 +1000

    Add nvim-treesitter-context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,48 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    require 'nvim-treesitter.configs'.setup {
-      autotag = {
-        enable = true,
-      }
-    }
-    if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, {
-        "astro",
-        "bash",
-        "css",
-        "gitignore",
-        "jsdoc",
-        "json",
-        "json5",
-        "jsonc",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "regex",
-        "scss",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter-context" },
+    opts = function(_, opts)
+      require("nvim-treesitter.configs").setup({
+        autotag = {
+          enable = true,
+        },
       })
-    end
-  end,
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          "astro",
+          "bash",
+          "css",
+          "gitignore",
+          "jsdoc",
+          "json",
+          "json5",
+          "jsonc",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "regex",
+          "scss",
+          "toml",
+          "tsx",
+          "typescript",
+          "vim",
+        })
+      end
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = { "BufReadPost", "BufNewFile" },
+    keys = {
+      {
+        "[c",
+        function()
+          require("treesitter-context").go_to_context()
+        end,
+        silent = true,
+        desc = "Go to Context",
+      },
+    },
+  },
 }
