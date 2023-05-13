14d45c5f2ce01cac45bb71932ba37035985e3251
Iain Simmons <emptiful@gmail.com>
2023-05-14 09:05:12 +1000

    Disable word wrap on hosts file

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_custom_" .. name, { clear = true })
+end
+
+-- disable wrapping and spell check for hosts file
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  group = augroup("nowrap_hosts"),
+  pattern = { "/etc/hosts" },
+  callback = function()
+    vim.opt_local.wrap = false
+    vim.opt_local.spell = false
+  end,
+})
