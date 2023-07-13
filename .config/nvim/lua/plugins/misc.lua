c77d528980fed5635925c90e2a3a75e755feeaa0
Iain Simmons <emptiful@gmail.com>
2023-07-14 07:07:07 +1000

    Register vim-abolish keymaps in which-key

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -39,5 +39,23 @@ return {
   {
     "tpope/vim-abolish",
     event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("which-key").register({
+        cr = {
+          name = "+coercion",
+          s = { desc = "Snake Case" },
+          _ = { desc = "Snake Case" },
+          m = { desc = "Mixed Case" },
+          c = { desc = "Camel Case" },
+          u = { desc = "Snake Upper Case" },
+          U = { desc = "Snake Upper Case" },
+          k = { desc = "Kebab Case" },
+          t = { desc = "Title Case (not reversible)" },
+          ["-"] = { desc = "Kebab Case (not reversible)" },
+          ["."] = { desc = "Dot Case (not reversible)" },
+          ["<space>"] = { desc = "Space Case (not reversible)" },
+        },
+      })
+    end,
   },
 }
