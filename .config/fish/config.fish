4eec5a8649d4a55b3907019a7849785fb5eb2572
Iain Simmons <emptiful@gmail.com>
2023-07-17 12:21:41 +1000

    Migrate zsh config to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,6 +15,7 @@ fish_add_path /usr/local/bin
 eval (/usr/local/bin/brew shellenv)
 starship init fish | source # https://starship.rs/
 zoxide init fish | source # 'ajeetdsouza/zoxide'
+direnv hook fish | source # direnv
 
 set -U fish_greeting # disable fish greeting
 set -U fish_key_bindings fish_vi_key_bindings
@@ -26,6 +27,36 @@ set -Ux EDITOR nvim # 'neovim/neovim' text editor
 # set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
 # set -Ux PAGER "~/.local/bin/nvimpager" # 'lucc/nvimpager'
 set -Ux VISUAL nvim
+
+# env vars
+set -gx XDG_CONFIG_HOME "/Users/isimmons/.config"
+
+set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden --exclude .git --color=always"
+set -gx FZF_DEFAULT_OPTS --ansi
+set -gx FZF_TMUX_OPTS -p
+set -gx FZF_CTRL_R_OPTS "--reverse --preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
+
+fish_add_path "$HOME/.rvm/bin"
+fish_add_path "$HOME/.pyenv/bin"
+# eval (pyenv init --path)
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
+set -gx VOLTA_HOME "$HOME/.volta"
+fish_add_path "$VOLTA_HOME/bin"
+set -gx PNPM_HOME /Users/isimmons/Library/pnpm
+fish_add_path "$PNPM_HOME"
+fish_add_path "$HOME/.yarn/bin"
+fish_add_path "$HOME/.config/yarn/global/node_modules/.bin"
+
+# go
+fish_add_path /usr/local/go/bin
+
+# ni custom configuration file path
+set -gx NI_CONFIG_FILE "$HOME/.nirc"
+
+function tm
+    tmux a -t tmux || exec tmux new -s tmux && exit
+end
 
 function mcd # Makes new Dir and jumps inside
     mkdir -p "$1" && cd "$1"
@@ -43,6 +74,7 @@ function sshkey # generate ssh key in directory
     mkdir -p "$1" && cd "$1" && ssh-keygen -t rsa -N '' -f cid_rsa
 end
 
+abbr myip "ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
 abbr cp 'cp -iv' # Preferred 'cp' implementation
 abbr mv 'mv -iv' # Preferred 'mv' implementation
 abbr mkdir 'mkdir -pv' # Preferred 'mkdir' implementation
