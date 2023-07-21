b6a9cb08d0d91147fa654ad511256fd344b23f89
Iain Simmons <emptiful@gmail.com>
2023-07-21 10:53:30 +1000

    Change fnm dir

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -1,6 +1,6 @@
 # fnm
 set PATH "/Users/isimmons/Library/Application\ Support/fnm" $PATH
 if type fnm -q && status is-interactive
-    fnm env --shell fish --use-on-cd | source
+    fnm env --shell fish --use-on-cd --fnm-dir ~/.fnm | source
     fnm completions --shell fish | source
 end
