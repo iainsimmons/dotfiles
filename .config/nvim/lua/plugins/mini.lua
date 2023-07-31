5d68c421f4a235d94209bcefc2e690326419dd5b
Iain Simmons <emptiful@gmail.com>
2023-07-31 14:27:12 +1000

    Add Neotree back in

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,15 @@
+return {
+  "echasnovski/mini.files",
+  opts = {
+    windows = {
+      preview = true,
+      width_focus = 30,
+      width_preview = 30,
+    },
+    options = {
+      -- Whether to use for editing directories
+      -- Disabled by default in LazyVim because neo-tree is used for that
+      use_as_default_explorer = true,
+    },
+  },
+}
