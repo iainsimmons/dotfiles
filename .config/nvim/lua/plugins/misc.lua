cd3d97ea24ff623b9ad3dd04e7b3ed403a963646
Iain Simmons <emptiful@gmail.com>
2023-09-07 15:52:09 +1000

    Add rayso.nvim
    For generating quick code screenshots with https://ray.so

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -95,4 +95,29 @@ return {
       highlight_for_count = true,
     },
   },
+  {
+    "TobinPalmer/rayso.nvim",
+    cmd = "Rayso",
+    opts = {
+      open_cmd = "Arc",
+      options = {
+        background = true, -- If the screenshot should have a background.
+        dark_mode = true, -- If the screenshot should be in dark mode.
+        logging_path = "", -- Path to create a log file in.
+        logging_file = "rayso", -- Name of log file, will be a markdown file, ex rayso.md.
+        logging_enabled = false, -- If you enable the logging file.
+        padding = 32, -- The default padding that the screenshot will have.
+        theme = "raindrop", -- Theme
+      },
+    },
+    keys = {
+      {
+        "<leader>rs",
+        ":Rayso<cr>",
+        desc = "Share with Ray.so",
+        silent = true,
+        mode = { "n", "v" },
+      },
+    },
+  },
 }
