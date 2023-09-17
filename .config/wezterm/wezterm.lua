66ad052ea6e85276ab1b5ddd6de4dd90cea874d9
Iain Simmons <emptiful@gmail.com>
2023-09-17 12:33:10 +1000

    Maximise WezTerm on startup
    And remove current session from tmux status

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,16 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
+local mux = wezterm.mux
+
+wezterm.on("gui-attached", function()
+	-- maximize all displayed windows on startup
+	local workspace = mux.get_active_workspace()
+	for _, window in ipairs(mux.all_windows()) do
+		if window:get_workspace() == workspace then
+			window:gui_window():maximize()
+		end
+	end
+end)
 
 local function multiple_actions(keys)
 	local actions = {}
