fa5a11d415deca590443d462e6f6faf326720895
Iain Simmons <emptiful@gmail.com>
2023-10-03 19:45:56 +1000

    Update indent-blankline config and colours

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,14 +6,6 @@ vim.opt.wrap = true
 vim.opt.conceallevel = 0
 vim.opt.diffopt = "internal,filler,closeoff,vertical"
 
--- add highlight colours for indent blankline
-vim.cmd([[highlight IndentBlanklineIndent1 guifg=#ff007c gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent2 guifg=#ff9e64 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent3 guifg=#bb9af7 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent4 guifg=#9ece6a gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent5 guifg=#1abc9c gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent6 guifg=#f7768e gui=nocombine]])
-
 -- Add .psv extension for Pipe Separated Values format
 vim.filetype.add({
   pattern = {
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -14,12 +14,6 @@ return {
             Visual = {
               bg = darken(colors.selection, 15),
             },
-            IndentBlanklineIndent1 = { fg = "#ff007c" },
-            IndentBlanklineIndent2 = { fg = "#ff9e64" },
-            IndentBlanklineIndent3 = { fg = "#bb9af7" },
-            IndentBlanklineIndent4 = { fg = "#9ece6a" },
-            IndentBlanklineIndent5 = { fg = "#1abc9c" },
-            IndentBlanklineIndent6 = { fg = "#f7768e" },
           }
         end,
       })
@@ -35,19 +29,62 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      char = "│",
-      space_char_blankline = " ",
-      char_highlight_list = {
-        -- see .config/nvim/lua/config/options.lua
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-      },
-    },
+    main = "ibl",
+    config = function()
+      local ibl = require("ibl")
+      local hooks = require("ibl.hooks")
+      local colors = {
+        cyan = "#61E2FF",
+        green = "#72f1b8",
+        orange = "#ff8b39",
+        pink = "#FC199A",
+        purple = "#AF6DF9",
+        red = "#fe4450",
+        yellow = "#FFCC00",
+        blankline = "#57367C",
+        active_blankline = "#7E0C4D",
+        inlay_hint = "#8C57C7",
+      }
+
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "IndentBlanklineIndent1", { fg = colors.red })
+        vim.api.nvim_set_hl(0, "IndentBlanklineIndent2", { fg = colors.orange })
+        vim.api.nvim_set_hl(0, "IndentBlanklineIndent3", { fg = colors.yellow })
+        vim.api.nvim_set_hl(0, "IndentBlanklineIndent4", { fg = colors.green })
+        vim.api.nvim_set_hl(0, "IndentBlanklineIndent5", { fg = colors.cyan })
+        vim.api.nvim_set_hl(0, "IndentBlanklineIndent6", { fg = colors.purple })
+        vim.api.nvim_set_hl(0, "IndentBlanklineActive", { fg = colors.active_blankline })
+      end)
+      ibl.setup({
+        indent = {
+          char = "│",
+          tab_char = "│",
+          highlight = {
+            "IndentBlanklineIndent1",
+            "IndentBlanklineIndent2",
+            "IndentBlanklineIndent3",
+            "IndentBlanklineIndent4",
+            "IndentBlanklineIndent5",
+            "IndentBlanklineIndent6",
+          },
+        },
+        scope = { enabled = true, highlight = "IndentBlanklineActive" },
+        exclude = {
+          filetypes = {
+            "help",
+            "alpha",
+            "dashboard",
+            "neo-tree",
+            "Trouble",
+            "lazy",
+            "mason",
+            "notify",
+            "toggleterm",
+            "lazyterm",
+          },
+        },
+      })
+    end,
   },
   {
     "brenoprata10/nvim-highlight-colors",
