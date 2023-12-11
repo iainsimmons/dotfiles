1ce3b0bf4500339031f73e08ba3f06a6c34d9129
Iain Simmons <emptiful@gmail.com>
2023-12-11 17:03:11 +1000

    Add cpf script to copy current file
    And nvim keymap <leader>cc

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -32,3 +32,5 @@ keymap.set(
 keymap.set("n", "<leader>ga", ":! git add -f %<CR>", { desc = "[G]it [A]dd (force) current file", silent = true })
 
 keymap.set("n", "<leader>uh", ":HighlightColorsToggle<CR>", { desc = "[H]ighlightColors Toggle", silent = true })
+
+keymap.set("n", "<leader>cc", ":! cpf %<CR>", { desc = "[C]opy [C]urrent file", silent = true })
