582f3197c4dd316e4dc83e1e6799dcafcfff29a8
Iain Simmons <emptiful@gmail.com>
2023-12-23 10:58:32 +1000

    Adjust LSP and use prettierd instead of prettier for formatting

---
+++ .config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,23 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      ["javascript"] = { "prettierd" },
+      ["javascriptreact"] = { "prettierd" },
+      ["typescript"] = { "prettierd" },
+      ["typescriptreact"] = { "prettierd" },
+      ["vue"] = { "prettierd" },
+      ["css"] = { "prettierd" },
+      ["scss"] = { "prettierd" },
+      ["less"] = { "prettierd" },
+      ["html"] = { "prettierd" },
+      ["json"] = { "prettierd" },
+      ["jsonc"] = { "prettierd" },
+      ["yaml"] = { "prettierd" },
+      ["markdown"] = { "prettierd" },
+      ["markdown.mdx"] = { "prettierd" },
+      ["graphql"] = { "prettierd" },
+      ["handlebars"] = { "prettierd" },
+    },
+  },
+}
