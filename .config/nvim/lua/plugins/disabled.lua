bee54081b5f75b1ffd1505406e82bad881b6c8db
Iain Simmons <emptiful@gmail.com>
2024-01-14 21:31:22 +1000

    Add Comment.nvim instead of mini.comment

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,5 +2,5 @@ return {
   { "folke/todo-comments.nvim", enabled = false, keys = {} },
   { "catppuccin", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
+  { "echasnovski/mini.comment", enabled = false }, -- in favour of Comment.nvim
 }
