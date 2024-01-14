f2395547bc3bd57afd4ab7f4c4d2408cb5322ec3
Iain Simmons <emptiful@gmail.com>
2024-01-15 00:21:46 +1000

    Add doc border to LSP via Noice

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -134,10 +134,11 @@ return {
         },
       },
       presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = true, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
       },
     },
   },
