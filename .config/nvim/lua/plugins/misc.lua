2a72a7baacf9b6f30b7475cf75bc80ad425f96fd
Iain Simmons <emptiful@gmail.com>
2024-02-12 10:15:17 +1000

    Add hurl.nvim to do REST requests from Neovim
    https://github.com/jellydn/hurl.nvim
    https://hurl.dev/

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -97,4 +97,39 @@ return {
     },
   },
   { "echasnovski/mini.bracketed", event = { "BufReadPost", "BufNewFile" }, version = false, opts = {} },
+  {
+    "jellydn/hurl.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    ft = "hurl",
+    opts = {
+      -- Show debugging info
+      debug = false,
+      -- Show notification on run
+      show_notification = false,
+      -- Show response in popup or split
+      mode = "split",
+      -- Default formatter
+      formatters = {
+        json = { "jq" }, -- Make sure you have install jq in your system, e.g: brew install jq
+        html = {
+          "prettier", -- Make sure you have install prettier in your system, e.g: npm install -g prettier
+          "--parser",
+          "html",
+        },
+      },
+      env_file = {
+        ".env",
+      },
+    },
+    keys = {
+      -- Run API request
+      { "<leader>A", "<cmd>HurlRunner<CR>", desc = "Run All requests" },
+      { "<leader>ra", "<cmd>HurlRunnerAt<CR>", desc = "Run Api request" },
+      { "<leader>te", "<cmd>HurlRunnerToEntry<CR>", desc = "Run Api request to entry" },
+      { "<leader>tm", "<cmd>HurlToggleMode<CR>", desc = "Hurl Toggle Mode" },
+      { "<leader>tv", "<cmd>HurlVerbose<CR>", desc = "Run Api in verbose mode" },
+      -- Run Hurl request in visual mode
+      { "<leader>rh", ":HurlRunner<CR>", desc = "Hurl Runner", mode = "v" },
+    },
+  },
 }
