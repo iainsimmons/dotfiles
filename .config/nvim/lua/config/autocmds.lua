abbff0743a84d7d6f43cb43804e927cc4d3719e0
Iain Simmons <emptiful@gmail.com>
2024-03-08 21:48:47 +1000

    Add fix for weird Telescope issue where it opens in insert mode

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -39,3 +39,13 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = { "*tmux.conf" },
   command = "execute 'silent !tmux source <afile> --silent'",
 })
+
+-- Fix for weird Telescope issue where it opens the file in insert mode
+-- https://github.com/nvim-telescope/telescope.nvim/issues/2027#issuecomment-1561836585
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
