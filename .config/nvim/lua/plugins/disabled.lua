735f4339884726622efc8fb07437aa51a19f8489
Iain Simmons <emptiful@gmail.com>
2024-04-15 20:07:37 +1000

    Try mini.files and mini.comment
    And remove Comment.nvim

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,4 @@
 return {
-  { "folke/todo-comments.nvim", enabled = false, keys = {} },
   { "catppuccin", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
-  { "echasnovski/mini.comment", enabled = false }, -- in favour of Comment.nvim
 }
--- .config/nvim/lua/plugins/substitute.lua
+++ .config/nvim/lua/plugins/substitute.lua
@@ -5,9 +5,9 @@ return {
     local substitute = require("substitute")
     substitute.setup()
     local keymap = vim.keymap
-    vim.keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
-    vim.keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-    vim.keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
-    vim.keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+    keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
+    keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
+    keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
+    keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
   end,
 }
