8e81ac85dc08162dfa838ce3c51a3be3e91b85ed
Iain Simmons <emptiful@gmail.com>
2024-09-29 20:05:02 +1000

    Set PATH for fnm

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -1,5 +1,5 @@
 # fnm
-set PATH "/Users/isimmons/Library/Application\ Support/fnm" $PATH
+set PATH /opt/homebrew/bin $PATH
 if type fnm -q && status is-interactive
     fnm env --shell fish --use-on-cd --fnm-dir ~/.fnm | source
     fnm completions --shell fish | source
