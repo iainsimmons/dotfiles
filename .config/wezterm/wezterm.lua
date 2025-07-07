0f7bae4d7969fc9ef459ce710118d37c084cc2c0
Iain Simmons <emptiful@gmail.com>
2025-07-07 15:04:11 +1000

    Add fish function to set user var and switch WezTerm workspace
    and corresponding WezTerm event handler

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -108,6 +108,21 @@ end)
 
 wezterm.on("gui-startup", resurrect.state_manager.resurrect_on_gui_startup)
 
+wezterm.on("user-var-changed", function(window, pane, name, value)
+  if name == "switch-workspace" then
+    local cmd_context = wezterm.json_parse(value)
+    window:perform_action(
+      act.SwitchToWorkspace({
+        name = cmd_context.workspace,
+        spawn = {
+          cwd = cmd_context.cwd,
+        },
+      }),
+      pane
+    )
+  end
+end)
+
 ------------
 -- Config --
 ------------
