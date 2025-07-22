cc5be492b47d607cb4762f70ec2ad266f58e252c
Iain Simmons <emptiful@gmail.com>
2025-07-23 00:41:26 +1000

    Update fish config for Arch Linux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,14 +8,11 @@
 # A smart and user-friendly command line
 # https://fishshell.com/
 # env vars
-set -gx XDG_CONFIG_HOME "/Users/isimmons/.config"
+set -gx XDG_CONFIG_HOME "/home/iain/.config"
 
-fish_add_path /opt/homebrew/bin
 fish_add_path /usr/local/bin
 fish_add_path $HOME/bin
 fish_add_path $HOME/go/bin
-
-eval (/opt/homebrew/bin/brew shellenv)
 
 source $XDG_CONFIG_HOME/fish/themes/tokyonight_night.fish
 
@@ -48,47 +45,36 @@ set fzf_git_log_opts --bind "ctrl-u:preview-half-page-up,ctrl-d:preview-half-pag
 set fzf_directory_opts --reverse --bind "ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down"
 set fzf_preview_dir_cmd lsd -aghl
 
-# slumber config
-set -gx SLUMBER_CONFIG_PATH "$XDG_CONFIG_HOME/slumber/config.yml"
-
 #fish_add_path "$HOME/.rvm/bin"
-set -gx PNPM_HOME /Users/isimmons/Library/pnpm
-fish_add_path "$PNPM_HOME"
-fish_add_path "$HOME/.yarn/bin"
-fish_add_path "$HOME/.config/yarn/global/node_modules/.bin"
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-fish_add_path "$BUN_INSTALL/bin"
-
-# go
-fish_add_path /usr/local/go/bin
-
+# set -gx PNPM_HOME /Users/isimmons/Library/pnpm
+# fish_add_path "$PNPM_HOME"
+# fish_add_path "$HOME/.yarn/bin"
+# fish_add_path "$HOME/.config/yarn/global/node_modules/.bin"
+# # bun
+# set --export BUN_INSTALL "$HOME/.bun"
+# set --export PATH $BUN_INSTALL/bin $PATH
+# fish_add_path "$BUN_INSTALL/bin"
+#
+# # go
+# fish_add_path /usr/local/go/bin
+#
 # rust
 fish_add_path "$HOME/.cargo/bin"
-
-# python
-fish_add_path "$HOME/.pyenv/shims"
-set -gx PYTHON python
-# eval (pyenv init --path)
-# status --is-interactive; and pyenv init - | source
-# status --is-interactive; and pyenv virtualenv-init - | source
-
-function tm
-    sesh connect dotfiles
-    # tmux a -t dotfiles || exec tmux new -c ~ -s dotfiles
-end
+#
+# # python
+# fish_add_path "$HOME/.pyenv/shims"
+# set -gx PYTHON python
+# # eval (pyenv init --path)
+# # status --is-interactive; and pyenv init - | source
+# # status --is-interactive; and pyenv virtualenv-init - | source
+#
+# function tm
+#     sesh connect dotfiles
+#     # tmux a -t dotfiles || exec tmux new -c ~ -s dotfiles
+# end
 
 function mcd # Makes new Dir and jumps inside
     mkdir -p "$1" && cd "$1"
-end
-
-function trash # Moves a file to the MacOS trash
-    command mv "$argv" ~/.Trash
-end
-
-function ql # Opens any file in MacOS Quicklook Preview
-    qlmanage -p "$argv" >&/dev/null
 end
 
 function sshkey # generate ssh key in directory
@@ -129,6 +115,10 @@ function compress_img -d "Compress images with ImageMagick" -a input_path -a out
     mogrify -path "$output_path" -filter Triangle -define filter:support=2 -thumbnail "$output_width" -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB "$input_path"
 end
 
+function open
+    xdg-open "$argv" >/dev/null 2>&1
+end
+
 abbr myip "ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
 abbr cp 'cp -iv' # Preferred 'cp' implementation
 abbr mv 'mv -iv' # Preferred 'mv' implementation
@@ -158,22 +148,21 @@ abbr za zoxide add
 abbr zz zi
 abbr find fd
 abbr y yazi
-# Hide/show all desktop icons (useful when presenting)
-abbr hidedesktop 'defaults write com.apple.finder CreateDesktop -bool false && killall Finder'
-abbr showdesktop 'defaults write com.apple.finder CreateDesktop -bool true && killall Finder'
+# # Hide/show all desktop icons (useful when presenting)
+# abbr hidedesktop 'defaults write com.apple.finder CreateDesktop -bool false && killall Finder'
+# abbr showdesktop 'defaults write com.apple.finder CreateDesktop -bool true && killall Finder'
 # delete item from history, use fzf to select
 abbr hd "history delete --exact --case-sensitive (history | fzf-tmux -p -m --reverse)"
 abbr obsidian_styles 'nvim ~/Dropbox/Obsidian\ Vault/.obsidian/snippets/styles.css'
 abbr bbd 'cd ~/dotfiles/ && brew bundle dump --force --describe'
-abbr admin '/Applications/Privileges.app/Contents/Resources/PrivilegesCLI --add'
-abbr privileges '/Applications/Privileges.app/Contents/Resources/PrivilegesCLI --add'
 abbr kulala '~/.local/share/nvim/lazy/kulala.nvim/lua/cli/kulala_cli.lua'
 
 alias c clear # c:            Clear terminal display
 alias cd z # use zoxide for cd (change directory)
-alias nvim 'TERM=wezterm /opt/homebrew/bin/nvim'
-alias v 'TERM=wezterm /opt/homebrew/bin/nvim'
+alias nvim 'TERM=wezterm /usr/bin/nvim'
+alias v 'TERM=wezterm /usr/bin/nvim'
 alias ls lsd # Use lsd instead of ls
 alias ll 'lsd -aghl' # Preferred 'ls'/'lsd' implementation
 alias svgo 'npx svgo --config $XDG_CONFIG_HOME/svgo.config.mjs'
 alias color termpicker
+alias yayf="yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% | xargs -ro yay -S"
