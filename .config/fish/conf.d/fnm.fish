df1044bb1bb85798fe787ef9297bf1cd979f68b9
Iain Simmons <emptiful@gmail.com>
2025-12-08 22:18:52 +1000

    Remove unused items from fish config

--- .config/fish/conf.d/fnm.fish
+++
@@ -1,6 +0,0 @@
-# fnm
-set PATH /opt/homebrew/bin $PATH
-if type fnm -q && status is-interactive
-    fnm env --shell fish --use-on-cd --fnm-dir ~/.fnm | source
-    fnm completions --shell fish | source
-end
