ad59c21481ad453ee0bb1fdfcd3bc49e26dd545a
Iain Simmons <emptiful@gmail.com>
2026-03-26 12:22:17 +1000

    Update script to set macOS preferences
    Disable press and hold to show accented characters.
    (gimme back my key-repeat!)

--- bin/macos-set-prefs.sh
+++ bin/macos-set-prefs.sh
@@ -2,3 +2,6 @@
 # Use Cmd + Opt + D to toggle show/hide if necessary
 defaults write com.apple.dock autohide-delay -float 1000
 killall Dock
+
+# Disable press and hold to show accented characters
+defaults write -g ApplePressAndHoldEnabled -bool false
