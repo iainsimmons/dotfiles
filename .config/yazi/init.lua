9aa86110a475725bf08ad64340c9841b8c539188
Iain Simmons <emptiful@gmail.com>
2026-09-11 22:21:15 +1000

    Fix mise dotfiles config for macOS
    Yazi and Kitty differences

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,1 +1,8 @@
-require("sshfs"):setup()
+local handle = io.popen("uname -s")
+local osname = handle and handle:read("*l")
+if handle then
+  handle:close()
+end
+if osname ~= "Darwin" then
+  require("sshfs"):setup()
+end
