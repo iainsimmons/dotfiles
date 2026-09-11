a316ba0c67780685cfb3932d721b5960b14c9d37
Iain Simmons <emptiful@gmail.com>
2026-09-11 23:04:38 +1000

    Fix rustup fish config

--- .config/fish/conf.d/rustup.fish
+++ .config/fish/conf.d/rustup.fish
@@ -1,5 +1,4 @@
-# macOS-only: rustup-managed cargo env. Inert on Linux (cargo is installed via
-# pacman there and added to PATH by config.fish).
-if test (uname) = Darwin && test -f "$HOME/.cargo/env.fish"
+# Linux-only: rustup-managed cargo env. Inert on macOS.
+if test (uname) != Darwin && test -f "$HOME/.cargo/env.fish"
     source "$HOME/.cargo/env.fish"
 end
